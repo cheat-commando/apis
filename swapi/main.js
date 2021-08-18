f2d5950f0115c2f6117f06068c220aa10963236a
Carston Work <carston.work@gmail.com>
2021-08-18 16:45:58 -0600

    Last little touches

--- swapi/main.js
+++ swapi/main.js
@@ -8,7 +8,6 @@ const getResidents = () => {
         console.log('exit')
         return
     }
-    currentPlanet.textContent = planetName.value.toUpperCase();
     let currentDisplay = document.querySelectorAll('h2');
     // removes the current list
     for (let i = 0; i < currentDisplay.length; i++) {
@@ -23,6 +22,7 @@ const getResidents = () => {
                 return
             }
             let residents = res.data.results[0].residents;
+            currentPlanet.textContent = res.data.results[0].name;
             if (residents.length === 0) {
                 const forShame = document.createElement('h2');
                 forShame.textContent = 'No residents available'
